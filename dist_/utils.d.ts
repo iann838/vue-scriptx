9083e42c29c603e67320387b47dacacb48dfd879
Paaksing <paaksingtech@gmail.com>
2024-10-21 18:29:52 -0400

    fix: migrate new npm account

--- dist_/utils.d.ts
+++
@@ -1,7 +0,0 @@
-export default class {
-    static isUndefined(x: any): boolean;
-    static pick(o: Record<string, any>, props: Array<string | number>): Record<string, any>;
-    static omit(o: Record<string, any>, props: Array<string | number>): Record<string, any>;
-    static omitBy(o: Record<string, any>, pred: CallableFunction): Record<string, any>;
-    static defaults(o: Record<string, any>, ...sources: Array<Record<string, any>>): void;
-}
